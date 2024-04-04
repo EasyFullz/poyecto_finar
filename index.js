eebed148aad76a786e43866c88c36f4e3bd7e4bb
EasyFullz <protelco6655@gmail.com>
2024-04-04 00:07:04 -0500

    se termino el carrusel de cards y se emepazo a implementar toda la parte del mapa interactivo

--- index.js
+++ index.js
@@ -19,27 +19,11 @@ import { inicializarSearch } from './scripts/search.js';
 
 inicializarSearch();
 
-document.addEventListener("DOMContentLoaded", function() {
-  const cardContainer = document.querySelector('.card-container');
-  const nextButton = document.querySelector('.next'); // Asegúrate de tener este botón en tu HTML
-  const prevButton = document.querySelector('.prev'); // Asegúrate de tener este botón en tu HTML
+import { cardCarrusel } from './scripts/cards.js';
 
-  // Considerando el ancho de la tarjeta y el gap
-  const cardWidth = 20; // Ancho de la tarjeta (rem)
-  const gap = 5; // Espacio entre tarjetas (rem)
-  const scrollAmount = cardWidth + gap; // Ajustar si es necesario
+cardCarrusel();
 
-  // Convertir rem a px para el desplazamiento
-  const scrollAmountPx = scrollAmount * parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-  nextButton.addEventListener('click', function() {
-      cardContainer.scrollBy({ left: scrollAmountPx, behavior: 'smooth' });
-  });
-
-  prevButton.addEventListener('click', function() {
-      cardContainer.scrollBy({ left: -scrollAmountPx, behavior: 'smooth' });
-  });
-});
 
 
 
---
+++ scripts/cards.js
@@ -0,0 +1,25 @@
+
+export const cardCarrusel = () => {
+    document.addEventListener("DOMContentLoaded", function() {
+        const cardContainer = document.querySelector('.card-container');
+        const nextButton = document.querySelector('.next');
+        const prevButton = document.querySelector('.prev'); 
+      
+        // Considerando el ancho de la tarjeta y el gap
+        const cardWidth = 100; // Ancho de la tarjeta (rem)
+        const gap = 5; // Espacio entre tarjetas (rem)
+        const scrollAmount = cardWidth + gap; // Ajustar si es necesario
+      
+        // Convertir rem a px para el desplazamiento
+        const scrollAmountPx = scrollAmount * parseFloat(getComputedStyle(document.documentElement).fontSize);
+      
+        nextButton.addEventListener('click', function() {
+            cardContainer.scrollBy({ left: scrollAmountPx, behavior: 'smooth' });
+        });
+      
+        prevButton.addEventListener('click', function() {
+            cardContainer.scrollBy({ left: -scrollAmountPx, behavior: 'smooth' });
+        });
+    });
+}
+
